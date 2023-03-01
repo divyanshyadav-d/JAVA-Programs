a60eed865ceacebe3cdb31e42e22b28309e31f24
divyanshyadav-d <73026210+divyanshyadav-d@users.noreply.github.com>
2023-03-01 21:44:19 +0530

    Create Read & Print Interger.java

---
+++ Read & Print Interger.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class ReadandPrintInterger {
+    public static void main( String[] args ) {
+        
+        System.out.println("Enter the number ");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        System.out.println("The input number is " + num);
+
+        
+    }
+}
